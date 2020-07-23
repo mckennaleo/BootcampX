badbd9360313c1e6c1648754d67967665f1a40bc
mckennaleo <mckenna.leo@gmail.com>
2020-07-23 11:44:25 -0400

    sql injection prevention

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,10 +1,33 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'lkm',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+const cohortName = process.argv[2];
+// Store all potentially malicious values in an array. 
+const values = [`%${cohortName}%`];
+
+
+
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = like $1
 ORDER BY teacher;
 `)
   .then(res => {
